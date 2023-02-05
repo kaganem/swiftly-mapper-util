5178b8fad2b6e02b7a3b539e632626b5717f8bc3
Ed Kagan <5306834+kaganem@users.noreply.github.com>
2023-02-05 02:18:54 +0200

    Traefik-wrapped env, to test outside

--- src/main/java/com/swiftly/mapper/GreetingResource.java
+++ src/main/java/com/swiftly/mapper/GreetingResource.java
@@ -1,16 +1,34 @@
 package com.swiftly.mapper;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class GreetingResource {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-    @GET
+@Path("/coordinates")
+public class GreetingResource {
+    public static class CoordinatesSnapshot {
+
+        @JsonProperty("LON")
+        public double longitude;
+
+        @JsonProperty("LAT")
+        public double latitude;
+
+        @JsonProperty("T")
+        public long timestamp;
+    }
+
+    @POST
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public String hello(CoordinatesSnapshot coordinatesSnapshot) {
+        System.out.println(coordinatesSnapshot.longitude
+                + " x " +
+                coordinatesSnapshot.latitude
+                + " : " +
+                coordinatesSnapshot.timestamp);
+        return "";
     }
 }
